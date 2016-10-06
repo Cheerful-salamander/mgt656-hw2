928447814f422c7b71bd4dff4c873445727291dc
Anastasia Kiryushkina <anastasia.kiryushkina@yale.edu>
2016-10-06 23:57:08 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Greetings from New Haven!');
 });
 
 app.listen(process.env.PORT || 4000);
